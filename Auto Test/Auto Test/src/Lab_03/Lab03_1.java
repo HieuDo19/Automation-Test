fac8dab5507c62cbb79551cdb6ebc17100f7b741
HieuDo19 <hieu.dd.tlu@gmail.com>
2023-09-29 02:07:32 +0700

    .

---
+++ Auto Test/Auto Test/src/Lab_03/Lab03_1.java
@@ -0,0 +1,20 @@
+package Lab_03;
+
+public class Lab03_1 {
+    public static void main(String[] args) {
+        int[] myIntArr = new int[] {1, 2, 3, 4, 5};
+        int countEvenNum = 0;
+        int countOddNum = 0;
+
+        for (int index = 0; index < myIntArr.length; index++) {
+            if (myIntArr[index]%2 == 0) {
+                countEvenNum++;
+            } else {
+                countOddNum++;
+            }
+        }
+
+        System.out.printf("Even numbers: %d", countEvenNum);
+        System.out.printf("Odd numbers: %d", countOddNum);
+    }
+}
