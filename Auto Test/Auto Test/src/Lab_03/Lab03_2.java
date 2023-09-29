6497ca051279c625e908fadf8a0b7cbcf0e60d5c
HieuDo19 <hieu.dd.tlu@gmail.com>
2023-09-29 08:59:33 +0700

    lab03_2

--- Auto Test/Auto Test/src/Lab_03/Lab03_2.java
+++ Auto Test/Auto Test/src/Lab_03/Lab03_2.java
@@ -1,4 +1,19 @@
 package lab_03;
 
 public class Lab03_2 {
+    public static void main(String[] args) {
+        int[] intArr = {1, 2, 3, 4, 5, 10};
+        int max = intArr [0];
+        int min = intArr [0];
+        for (int index = 0; index < intArr.length; index++) {
+            if (intArr[index] > max) {
+                max = intArr[index];
+            }
+            else if (min > intArr[index]) {
+                min = intArr[index];
+            }
+        }
+        System.out.println(max);
+        System.out.println(min);
+    }
 }
