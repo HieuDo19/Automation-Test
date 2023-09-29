6ee957d1f260994c7f294b0240ec89f7b1fc3d0c
HieuDo19 <hieu.dd.tlu@gmail.com>
2023-09-29 23:10:53 +0700

    mindset lab03_3.

--- Auto Test/Auto Test/src/Lab_03/lab03_3.java
+++ Auto Test/Auto Test/src/Lab_03/lab03_3.java
@@ -1,2 +1,15 @@
-package lab_03;public class lab03_3 {
+package lab_03;
+
+public class lab03_3 {
+
+   /** Nhap mot day cac chu so bat ky
+    * Gan chu so dau tien index = 0
+    * Gan chu so thu hai index = 1
+    * So sanh "index = 0" va "index = 1" voi nhau, so nao nho hon dung truoc
+    * Tien hanh so sanh cac chu so con lai voi "index = 0" va "index = 1"
+    * Neu chu so duoc so sanh lon hon "index = 0" va "index = 1" thi se xep sau
+    * Neu chu so duoc so sanh nho hon "index = 0" va "index = 1" thi se xep truoc
+    * Truong hop con lai thi xep giua "index = 0" va "index = 1"
+    * So sanh cac chu so lan luot voi nhau, den khi day so duoc sap xep theo dung logic: min to max
+    */
 }
